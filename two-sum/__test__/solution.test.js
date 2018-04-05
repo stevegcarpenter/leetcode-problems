651024ec468673a2563e2371dd17fbe0ea76c6fc
Steven Carpenter <stevegcarpenter@gmail.com>
2018-04-05 06:01:47 -0700

    Added additional tests for edge cases

--- two-sum/__test__/solution.test.js
+++ two-sum/__test__/solution.test.js
@@ -10,11 +10,19 @@ describe('#twosum', () => {
     it('should still work with negative values', () => {
       expect(twosum([-1, 2, 3, 4, 5, 6], 1)).toEqual([0, 1]);
     });
+
+    it('should return an empty array if no two valid sums could be found', () => {
+      expect(twosum([23, 5, 100, 33, 49, 344, 888], 15000)).toEqual([]);
+    });
   });
 
   describe('Invalid input', () => {
     it('should return back an empty array when provided an empty array', () => {
       expect(twosum([], 10)).toEqual([]);
+    });
+
+    it('should return back and empty array when no sum value was provided', () => {
+      expect(twosum([1, 2, 3, 4, 5, 6])).toEqual([]);
     });
   });
 });
